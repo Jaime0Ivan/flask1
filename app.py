d55912e733128ad6084018ecc13f9dfe435c2c72
Jaime0Ivan <jaimeivan555@gmail.com>
2024-06-06 00:43:47 -0600

    se agregaron datos imagen y formulario

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hola Mundo'
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    group = request.form.get('group')
+    return f"Nombre: {name}, Grupo: {group}"
 
 if __name__ == '__main__':
     app.run(debug=True)
