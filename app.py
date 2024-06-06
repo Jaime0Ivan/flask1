00b72e704529dacadd9222a0cd2cef6c36c21b9e
Jaime0Ivan <jaimeivan555@gmail.com>
2024-06-06 14:43:18 -0600

    se publica el modelo ML de insectos

--- app.py
+++ app.py
@@ -1,16 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template, jsonify
+import joblib
+import pandas as pd
+import logging
 
 app = Flask(__name__)
 
+# Configurar el registro
+logging.basicConfig(level=logging.DEBUG)
+
+# Cargar el modelo entrenado
+model = joblib.load('modelo.pkl')
+app.logger.debug('Modelo cargado correctamente.')
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('formulario.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    name = request.form.get('name')
-    group = request.form.get('group')
-    return f"Nombre: {name}, Grupo: {group}"
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Obtener los datos enviados en el request
+        abdomen = float(request.form['abdomen'])
+        antena = float(request.form['antena'])
+        
+        # Crear un DataFrame con los datos
+        data_df = pd.DataFrame([[abdomen, antena]], columns=['abdomen', 'antena'])
+        app.logger.debug(f'DataFrame creado: {data_df}')
+        
+        # Realizar predicciones
+        prediction = model.predict(data_df)
+        app.logger.debug(f'Predicción: {prediction[0]}')
+        
+        # Devolver las predicciones como respuesta JSON
+        return jsonify({'categoria': prediction[0]})
+    except Exception as e:
+        app.logger.error(f'Error en la predicción: {str(e)}')
+        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
