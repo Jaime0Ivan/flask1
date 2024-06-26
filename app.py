8317d96f13cc84c648da983fede33ef7044f8c94
Jaime0Ivan <jaimeivan555@gmail.com>
2024-06-25 23:01:17 -0600

    Revert "se publica el modelo ML de insectos"
    This reverts commit 00b72e704529dacadd9222a0cd2cef6c36c21b9e.

--- app.py
+++ app.py
@@ -1,41 +1,16 @@
-from flask import Flask, request, render_template, jsonify
-import joblib
-import pandas as pd
-import logging
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Configurar el registro
-logging.basicConfig(level=logging.DEBUG)
-
-# Cargar el modelo entrenado
-model = joblib.load('modelo.pkl')
-app.logger.debug('Modelo cargado correctamente.')
-
 @app.route('/')
 def home():
-    return render_template('formulario.html')
+    return render_template('index.html')
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    try:
-        # Obtener los datos enviados en el request
-        abdomen = float(request.form['abdomen'])
-        antena = float(request.form['antena'])
-        
-        # Crear un DataFrame con los datos
-        data_df = pd.DataFrame([[abdomen, antena]], columns=['abdomen', 'antena'])
-        app.logger.debug(f'DataFrame creado: {data_df}')
-        
-        # Realizar predicciones
-        prediction = model.predict(data_df)
-        app.logger.debug(f'Predicción: {prediction[0]}')
-        
-        # Devolver las predicciones como respuesta JSON
-        return jsonify({'categoria': prediction[0]})
-    except Exception as e:
-        app.logger.error(f'Error en la predicción: {str(e)}')
-        return jsonify({'error': str(e)}), 400
+@app.route('/submit', methods=['POST'])
+def submit():
+    name = request.form.get('name')
+    group = request.form.get('group')
+    return f"Nombre: {name}, Grupo: {group}"
 
 if __name__ == '__main__':
     app.run(debug=True)
